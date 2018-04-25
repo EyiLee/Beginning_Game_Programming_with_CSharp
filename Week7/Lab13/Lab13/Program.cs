f9080c1e1c06895982b5e9dc4a3c81f0e749cae7
EyiLee <sdses9001@gmail.com>
2018-04-25 23:13:14 +0800

    [add] Lab13, Lab14

---
+++ Week7/Lab13/Lab13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCards;
+
+namespace Lab13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            deck.Shuffle();
+
+            List<Card> cards = new List<Card>();
+
+            // take 5 cards from deck
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(deck.TakeTopCard());
+            }
+
+            // print cards on hand
+            foreach (Card card in cards)
+            {
+                card.Print();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week7/Lab14/Lab14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // list of data
+            List<int> data = new List<int>();
+
+            // read number from user input
+            int input = 0;
+            while (input != -1)
+            {
+                input = int.Parse(Console.ReadLine());
+
+                if (input != -1)
+                {
+                    // add user input into the list
+                    data.Add(input);
+                }
+            }
+
+            // find the maximum value in the list
+            int maximum = 0;
+            foreach (int num in data)
+            {
+                if (num > maximum)
+                {
+                    maximum = num;
+                }
+            }
+            Console.WriteLine("Maximum value in the list: {0}", maximum);
+
+            // calculate the average of the values in the list
+            int sum = 0;
+            foreach (int num in data)
+            {
+                sum += num;
+            }
+            Console.WriteLine("Average value of the list: {0}", sum / data.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
